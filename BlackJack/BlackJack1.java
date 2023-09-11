001109ca084161ea18a52fb52a94d586fb615c2e
Thamilarasan C <thamilarasan.c2003@gmail.com>
2023-09-11 14:03:35 +0530

    BJ

--- BlackJack/BlackJack1.java
+++ BlackJack/BlackJack1.java
@@ -1,4 +1,3 @@
-
 package BlackJack;
 
 import java.util.*;
@@ -9,72 +8,17 @@ interface BlackJackInterface1 {
 
     public void buildDeck(int n);
 
-    public void shuffle(Deck d1);
+    public void shuffle();
 
     public int draw();
 
-    public boolean playerTurn(Player p1, ArrayList<Boolean> gameOn);
+    public boolean playerTurn(ArrayList<Boolean> gameOn);
 
-    public boolean dealerTurn(Dealer d1, ArrayList<Boolean> gameOn);
+    public boolean dealerTurn(ArrayList<Boolean> gameOn);
 
-    public void playerhits(Player p1);
+    public void playerhits();
 
     public boolean isBust(int sum);
-
-    public void checkWinner(Player p1, Dealer d1);
-
-}
-
-class Dealer {
-    Dealer() {
-    }
-
-    int dealerId;
-    int dealerSum = 0;
-    ArrayList<Integer> dealerCards = new ArrayList<Integer>();
-}
-
-class Player {
-    Player() {
-    }
-
-    int playerId;
-    int playerSum = 0;
-    ArrayList<Integer> playerCards = new ArrayList<Integer>();
-}
-
-class Deck {
-    int numberOfDecks;
-    Random random = new Random();
-    ArrayList<Integer> deck = new ArrayList<>();
-
-    Deck(int n) {
-        numberOfDecks = n;
-        buildDeck();
-    }
-
-    public void buildDeck() {
-        for (int n = 1; n <= numberOfDecks; n++) {
-            for (int i = 1; i <= 4; i++) {
-                for (int j = 2; j <= 11; j++)
-                    deck.add(j);
-            }
-        }
-    }
-
-    public int size() {
-        return deck.size();
-    }
-
-    public void shuffle(Deck d1) {
-        for (int currentIndex = 0; currentIndex < deck.size(); currentIndex++) {
-            int currentcard = deck.get(currentIndex);
-            int randomIndex = random.nextInt(d1.deck.size());
-            int randomCard = deck.get(randomIndex);
-            deck.set(currentIndex, randomCard);
-            deck.set(randomIndex, currentcard);
-        }
-    }
 
 }
 
@@ -85,12 +29,123 @@ public class BlackJack1 implements BlackJackInterface1 {
     int numberOfDecks;
     Scanner sc = new Scanner(System.in);
     Random random = new Random();
-
     Deck d1;
 
     public BlackJack1() {
         System.out.println("Welcome to blackjack game");
         getInputs();
+    }
+
+    public class Dealer {
+        Dealer() {
+        }
+
+        int dealerId;
+        int dealerSum = 0;
+        ArrayList<Integer> dealerCards = new ArrayList<Integer>();
+
+        public boolean dealerTurn(ArrayList<Boolean> gameOn) {
+            if (dealerSum >= winningPoint - 5) {
+                System.out.println("Dealer stays");
+                return false;
+            }
+            System.out.println("Dealer chooses to hit");
+            int card = d1.draw();
+            dealerCards.add(card);
+            dealerSum += card;
+            System.out.println("He draws a " + card + "\nHis total is " + dealerSum);
+            if (isBust(dealerSum)) {
+                System.out.println("Dealer Bust");
+                System.out.println("YOU WIN");
+                gameOn.set(0, false);
+                return false;
+            }
+            if (isWin(dealerSum)) {
+                System.out.println("Dealer Scored a winning point");
+                System.out.println("DEALER WINS!");
+                gameOn.set(0, false);
+                return false;
+            }
+            return true;
+        }
+    }
+
+    public class Player {
+        Player() {
+        }
+
+        int playerId;
+        int playerSum = 0;
+        ArrayList<Integer> playerCards = new ArrayList<Integer>();
+
+        public void playerhits() {
+            int card = d1.draw();
+            playerCards.add(card);
+            playerSum += card;
+            System.out.println("You drew a " + card);
+            System.out.println("Your total is " + playerSum);
+        }
+
+        public boolean playerTurn(ArrayList<Boolean> gameOn) {
+            System.out.println("Would you like to \"hit\" or \"stay\"");
+            System.out.println("Enter h for hit or s for stay");
+            char c = sc.next().charAt(0);
+            if (c == 'h') {
+                playerhits();
+                if (isBust(playerSum)) {
+                    System.out.println("You Bust");
+                    System.out.println("DEALER WINS!");
+                    gameOn.set(0, false);
+                    return false;
+                }
+                if (isWin(playerSum)) {
+                    System.out.println("You Scored a winning point");
+                    System.out.println("YOU WIN!");
+                    gameOn.set(0, false);
+                    return false;
+                }
+                return true;
+            }
+            return false;
+        }
+    }
+
+    public class Deck {
+        int numberOfDecks;
+        Random random = new Random();
+        ArrayList<Integer> deck = new ArrayList<>();
+
+        Deck(int n) {
+            numberOfDecks = n;
+            buildDeck();
+        }
+
+        public void buildDeck() {
+            for (int n = 1; n <= numberOfDecks; n++) {
+                for (int i = 1; i <= 4; i++) {
+                    for (int j = 2; j <= 11; j++)
+                        deck.add(j);
+                }
+            }
+        }
+
+        public void shuffle() {
+            for (int currentIndex = 0; currentIndex < deck.size(); currentIndex++) {
+                int currentcard = deck.get(currentIndex);
+                int randomIndex = random.nextInt(deck.size());
+                int randomCard = deck.get(randomIndex);
+                deck.set(currentIndex, randomCard);
+                deck.set(randomIndex, currentcard);
+            }
+        }
+
+        public int draw() {
+            int randomIndex = random.nextInt(deck.size());
+            int randomCard = deck.get(randomIndex);
+            deck.remove(randomIndex);
+            return randomCard;
+        }
+
     }
 
     public void getInputs() {
@@ -111,18 +166,18 @@ public class BlackJack1 implements BlackJackInterface1 {
         System.out.println("The game is Started");
 
         System.out.println(d1.deck);
-        d1.shuffle(d1);
+        d1.shuffle();
 
         p1 = new Player();
         d = new Dealer();
 
         for (int i = 0; i < 2; i++) {
 
-            int cardForPlayer = draw();
+            int cardForPlayer = d1.draw();
             p1.playerCards.add(cardForPlayer);
             p1.playerSum += cardForPlayer;
 
-            int cardForDealer = draw();
+            int cardForDealer = d1.draw();
             d.dealerCards.add(cardForDealer);
             d.dealerSum += cardForDealer;
 
@@ -138,7 +193,7 @@ public class BlackJack1 implements BlackJackInterface1 {
         boolean firstTurn = true;
 
         while (playerturn) {
-            playerturn = playerTurn(p1, gameOn);
+            playerturn = p1.playerTurn(gameOn);
         }
 
         while (gameOn.get(0) && dealerturn) {
@@ -149,7 +204,7 @@ public class BlackJack1 implements BlackJackInterface1 {
                 System.out.println("His hidden card was " + d.dealerCards.get(1) +
                         "\nHis total is " + d.dealerSum);
             }
-            dealerturn = dealerTurn(d, gameOn);
+            dealerturn = d.dealerTurn(gameOn);
         }
 
         if (gameOn.get(0))
@@ -170,36 +225,6 @@ public class BlackJack1 implements BlackJackInterface1 {
             System.out.println("Both of your points are equal.  TIE!  ");
     }
 
-    public int draw() {
-        int randomIndex = random.nextInt(d1.deck.size());
-        int randomCard = d1.deck.get(randomIndex);
-        d1.deck.remove(randomIndex);
-        return randomCard;
-    }
-
-    public boolean playerTurn(Player p1, ArrayList<Boolean> gameOn) {
-        System.out.println("Would you like to \"hit\" or \"stay\"");
-        System.out.println("Enter h for hit or s for stay");
-        char c = sc.next().charAt(0);
-        if (c == 'h') {
-            playerhits(p1);
-            if (isBust(p1.playerSum)) {
-                System.out.println("You Bust");
-                System.out.println("DEALER WINS!");
-                gameOn.set(0, false);
-                return false;
-            }
-            if (isWin(p1.playerSum)) {
-                System.out.println("You Scored a winning point");
-                System.out.println("YOU WIN!");
-                gameOn.set(0, false);
-                return false;
-            }
-            return true;
-        }
-        return false;
-    }
-
     public boolean isBust(int sum) {
         if (sum > winningPoint) {
             return true;
@@ -212,39 +237,6 @@ public class BlackJack1 implements BlackJackInterface1 {
             return true;
         }
         return false;
-    }
-
-    public void playerhits(Player p1) {
-        int card = draw();
-        p1.playerCards.add(card);
-        p1.playerSum += card;
-        System.out.println("You drew a " + card);
-        System.out.println("Your total is " + p1.playerSum);
-    }
-
-    public boolean dealerTurn(Dealer d1, ArrayList<Boolean> gameOn) {
-        if (d1.dealerSum >= winningPoint - 5) {
-            System.out.println("Dealer stays");
-            return false;
-        }
-        System.out.println("Dealer chooses to hit");
-        int card = draw();
-        d1.dealerCards.add(card);
-        d1.dealerSum += card;
-        System.out.println("He draws a " + card + "\nHis total is " + d1.dealerSum);
-        if (isBust(d1.dealerSum)) {
-            System.out.println("Dealer Bust");
-            System.out.println("YOU WIN");
-            gameOn.set(0, false);
-            return false;
-        }
-        if (isWin(d1.dealerSum)) {
-            System.out.println("Dealer Scored a winning point");
-            System.out.println("DEALER WINS!");
-            gameOn.set(0, false);
-            return false;
-        }
-        return true;
     }
 
     public static void main(String args[]) {
